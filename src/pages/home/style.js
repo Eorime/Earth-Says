e985f7070cb799eca1e27e96a974c4a37b684b33
Eorime <elenedgebuadze83@gmail.com>
2025-01-26 17:29:39 +0400

    Stylize fullscreen icons

--- src/pages/home/style.js
+++ src/pages/home/style.js
@@ -143,16 +143,33 @@ export const FullScreenContainer = styled.div`
 `;
 
 export const FullScreenIncrease = styled.div`
-	width: 20px;
-	height: 20px;
-	position: absolute;
-	cursor: pointer;
-	transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
-	transform-origin: center;
-	border: 2px solid rgb(102, 102, 102);
+	// width: 20px;
+	// height: 20px;
+	// border: 2px solid rgb(102, 102, 102);
+
 	z-index: 0;
 	left: 50%;
 	transform: translateX(-50%);
+	transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
+	cursor: pointer;
+	position: absolute;
+	transform-origin: center;
+
+	width: 24px;
+	height: 23px;
+
+	background: linear-gradient(to right, rgb(102, 102, 102) 2px, transparent 2px)
+			0 0,
+		linear-gradient(to right, rgb(102, 102, 102) 2px, transparent 2px) 0 100%,
+		linear-gradient(to left, rgb(102, 102, 102) 2px, transparent 2px) 100% 0,
+		linear-gradient(to left, rgb(102, 102, 102) 2px, transparent 2px) 100% 100%,
+		linear-gradient(to bottom, rgb(102, 102, 102) 2px, transparent 2px) 0 0,
+		linear-gradient(to bottom, rgb(102, 102, 102) 2px, transparent 2px) 100% 0,
+		linear-gradient(to top, rgb(102, 102, 102) 2px, transparent 2px) 0 100%,
+		linear-gradient(to top, rgb(102, 102, 102) 2px, transparent 2px) 100% 100%;
+
+	background-repeat: no-repeat;
+	background-size: 8px 8px;
 
 	&:hover {
 		width: 40px;
@@ -160,32 +177,47 @@ export const FullScreenIncrease = styled.div`
 		transform: translateX(-50%);
 	}
 
-	&:active {
-		border: 2px solid #d5d5d5;
-	}
+	// &:active {
+	// 	border: 2px solid #d5d5d5;
+	// }
 
 	//when clicked ichiteba sxva tipi
 `;
 
 export const FullScreenDecrease = styled.div`
-	width: 40px;
-	height: 20px;
+	// width: 40px;
+	// height: 20px;
 	position: absolute;
 	cursor: pointer;
 	transition: width 0.3s ease-in-out, height 0.3s ease-in-out;
 	transform-origin: center;
-	border: 2px solid rgb(102, 102, 102);
 	z-index: 0;
 	left: 50%;
 	transform: translateX(-50%);
 
+	width: 24px;
+	height: 23px;
+
+	background: linear-gradient(to left, rgb(102, 102, 102) 2px, transparent 2px)
+			0 0,
+		linear-gradient(to left, rgb(102, 102, 102) 2px, transparent 2px) 0 100%,
+		linear-gradient(to right, rgb(102, 102, 102) 2px, transparent 2px) 100% 0,
+		linear-gradient(to right, rgb(102, 102, 102) 2px, transparent 2px) 100% 100%,
+		linear-gradient(to top, rgb(102, 102, 102) 2px, transparent 2px) 0 0,
+		linear-gradient(to top, rgb(102, 102, 102) 2px, transparent 2px) 100% 0,
+		linear-gradient(to bottom, rgb(102, 102, 102) 2px, transparent 2px) 0 100%,
+		linear-gradient(to bottom, rgb(102, 102, 102) 2px, transparent 2px) 100%
+			100%;
+	background-repeat: no-repeat;
+	background-size: 7px 7px;
+
 	&:hover {
-		width: 20px;
+		width: 40px;
 		left: 50%;
 		transform: translateX(-50%);
 	}
 
-	&:active {
-		border: 2px solid #d5d5d5;
-	}
+	// &:active {
+	// 	border: 2px solid #d5d5d5;
+	// }
 `;
